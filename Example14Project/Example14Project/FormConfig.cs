2fa225c5a5c64bc4925198546b0e780d85545617
Sahlasaji <122045584+Sahlasaji@users.noreply.github.com>
2024-09-30 20:46:24 +0530

    Add files via upload

---
+++ Example14Project/Example14Project/FormConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Example14Project
+{
+    static class FormConfig
+    {
+        public static Window FrmSolid {  get; set; } = new SolidWindow();
+        public static Window FrmLinearGradient { get; set; } = new LinearGradientWindow();
+        public static Window FrmRadialGradient { get; set; } = new RadialGradientWindow();
+        public static Window FrmImage { get; set; } = new ImageWindow();
+        public static Window FrmVisual { get; set; } = new VisualWindow();
+
+
+        public static Window FrmStack { get; set; } = new StackWindow();
+        public static Window FrmWrapPanel { get; set; } = new WrapPanelWindow();
+        public static Window FrmCanvas { get; set; } = new CanvasWindow();
+        public static Window FrmGrid { get; set; } = new GridWindow();
+        public static Window FrmDock { get; set; } = new DockPanelWindow();
+
+
+    }
+}
---
+++ Example14Project/Example14Project/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Example14Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void btnSolidColor_Click(object sender, RoutedEventArgs e)
+        {
+            FormConfig.FrmSolid.ShowDialog();
+        }
+
+        private void btnLinear_Gradient_Click(object sender, RoutedEventArgs e)
+        {
+            FormConfig.FrmLinearGradient.Show();
+        }
+
+        private void btnRadial_Gradient_Click(object sender, RoutedEventArgs e)
+        {
+            FormConfig.FrmRadialGradient.Show();
+        }
+
+        private void btnImage_Click(object sender, RoutedEventArgs e)
+        {
+            FormConfig.FrmImage.Show();
+        }
+
+        private void btnVisual_Click(object sender, RoutedEventArgs e)
+        {
+            FormConfig.FrmVisual.Show();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnStack_Click(object sender, RoutedEventArgs e)
+        {
+            FormConfig.FrmStack.Show();
+        }
+
+        private void btnGrid_Click(object sender, RoutedEventArgs e)
+        {
+            FormConfig.FrmGrid.Show();
+        }
+
+        private void btnDockPanel_Click(object sender, RoutedEventArgs e)
+        {
+            FormConfig.FrmDock.Show(); 
+        }
+
+        private void btnCanvas_Click(object sender, RoutedEventArgs e)
+        {
+            FormConfig.FrmCanvas.Show();
+        }
+
+        private void btnWrapPanel_Click(object sender, RoutedEventArgs e)
+        {
+            FormConfig.FrmWrapPanel .Show();
+        }
+    }
+}
